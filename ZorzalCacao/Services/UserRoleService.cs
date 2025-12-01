c818f0957b1353f029fdf0bc682ee5dc02504bc3
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-01 09:34:54 -0400

    Feat: Se trabajo el service para los usuarios

---
+++ ZorzalCacao/Services/UserRoleService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ZorzalCacao.Data;
+
+namespace ZorzalCacao.Services;
+
+public interface IUserRoleService
+{
+    Task<List<ApplicationUser>> GetUsersByRoleAsync(string roleName);
+    Task<List<ApplicationUser>> GetProductoresAsync();
+    Task<List<ApplicationUser>> GetEmpleadosAsync();
+}
+
+public class UserRoleService : IUserRoleService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public UserRoleService(
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public async Task<List<ApplicationUser>> GetUsersByRoleAsync(string roleName)
+    {
+        var role = await _roleManager.FindByNameAsync(roleName);
+        if (role == null)
+            return new List<ApplicationUser>();
+
+        var usersInRole = await _userManager.GetUsersInRoleAsync(roleName);
+        return usersInRole.ToList();
+    }
+
+    public async Task<List<ApplicationUser>> GetProductoresAsync()
+    {
+        return await GetUsersByRoleAsync("Productor");
+    }
+
+    public async Task<List<ApplicationUser>> GetEmpleadosAsync()
+    {
+        return await GetUsersByRoleAsync("Empleado");
+    }
+}
+
