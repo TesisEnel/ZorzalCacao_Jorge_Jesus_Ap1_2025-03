dd254c795706b579ee9e30440909226b376feab5
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-03 22:46:03 -0400

    Chore: Migracion realizada correctamente

---
+++ ZorzalCacao/Migrations/20251204024440_CreandoSaco.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZorzalCacao.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreandoSaco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descripcion",
+                table: "Sacos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "Sacos",
+                columns: new[] { "SacoId", "CantidadPesada", "Descripcion" },
+                values: new object[] { 1, 0.0, "Saco estándar" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Sacos",
+                keyColumn: "SacoId",
+                keyValue: 1);
+
+            migrationBuilder.DropColumn(
+                name: "Descripcion",
+                table: "Sacos");
+        }
+    }
+}
