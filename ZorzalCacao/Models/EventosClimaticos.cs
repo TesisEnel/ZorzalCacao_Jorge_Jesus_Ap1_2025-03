4b293187b22b587eb2d63045df1c695369a3e17c
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2025-12-05 18:18:39 -0400

    Feat: Creacion del Modelo EventosClimaticos

---
+++ ZorzalCacao/Models/EventosClimaticos.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZorzalCacao.Models
+{
+    public class EventosClimaticos
+    {
+        [Key]
+        public int EventoClimaticoId { get; set; }
+
+        [Required(ErrorMessage = "La fecha es requerida")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "La zona es requerida")]
+        public string Zona { get; set; }
+
+        [Required(ErrorMessage = "El tipo de evento es requerido")]
+        public string TipoEvento { get; set; }
+        [Required(ErrorMessage = "La intensidad es requerida")]
+        public string Intensidad { get; set; } // leve, moderada, fuerte
+        public string Observaciones { get; set; }
+
+    }
+}
