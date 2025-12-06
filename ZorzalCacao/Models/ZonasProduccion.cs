998a3737df7aaa885e486d2db7d587c6500ae3cc
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-05 22:05:09 -0400

    Feat: Modelo creado con sus validaciones

---
+++ ZorzalCacao/Models/ZonasProduccion.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZorzalCacao.Models;
+
+public class ZonasProduccion
+{
+    [Key]
+    public int ZonaId { get; set; }
+    [Required(ErrorMessage = "Nombre de la zona requerido")]
+    public string Nombre { get; set; } = null!;
+
+    [Required(ErrorMessage = "Provincia requerida")]
+    public string Provincia { get; set; } = null!;
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "Distancia no valida")]
+    public double Distancia { get; set; }
+}
