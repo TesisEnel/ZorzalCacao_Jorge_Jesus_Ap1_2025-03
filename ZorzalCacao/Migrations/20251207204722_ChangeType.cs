49c2abaeb302ead14bd1d6a3995495998ae132c9
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-07 17:23:08 -0400

    Chore: Migracion realizada

---
+++ ZorzalCacao/Migrations/20251207204722_ChangeType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZorzalCacao.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CertificacionesProducto",
+                table: "Recogidas",
+                newName: "CertificacionProducto");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CertificacionProducto",
+                table: "Recogidas",
+                newName: "CertificacionesProducto");
+        }
+    }
+}
