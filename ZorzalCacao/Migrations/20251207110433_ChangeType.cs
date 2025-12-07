310ef9d1d3238f26633d24867d0e58e3eb4afeb2
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-07 07:14:09 -0400

    Chore: Migracion realizada despues del cambio de tipo

---
+++ ZorzalCacao/Migrations/20251207110433_ChangeType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZorzalCacao.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Cantidad",
+                table: "FermentacionesDetalles",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Cantidad",
+                table: "FermentacionesDetalles",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
