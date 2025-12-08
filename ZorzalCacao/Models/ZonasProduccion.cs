73ad24639b8c81dc8144e4fc3de342d577fc7fa2
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-08 13:19:59 -0400

    Feat: Validando de manera mas robusta el nombre y la provincia

--- ZorzalCacao/Models/ZonasProduccion.cs
+++ ZorzalCacao/Models/ZonasProduccion.cs
@@ -7,10 +7,14 @@ public class ZonasProduccion
 {
     [Key]
     public int ZonaId { get; set; }
+
     [Required(ErrorMessage = "Nombre de la zona requerido")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres.")]
     public string Nombre { get; set; } = null!;
 
     [Required(ErrorMessage = "Provincia requerida")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "La provincia debe tener entre 3 y 50 caracteres.")]
+    [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s.-]*$", ErrorMessage = "La provincia contiene caracteres no válidos.")]
     public string Provincia { get; set; } = null!;
 
     [Range(0.01, double.MaxValue, ErrorMessage = "Distancia no valida")]
