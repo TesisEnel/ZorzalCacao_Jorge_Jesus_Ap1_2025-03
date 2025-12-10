6cfb8d0fbba165dec6b1e6fc14e3c0ed8bcb6f84
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-09 20:52:19 -0400

    Fix: Cambiando requeridos por 'obligatorio'

--- ZorzalCacao/Models/EventosClimaticos.cs
+++ ZorzalCacao/Models/EventosClimaticos.cs
@@ -8,15 +8,15 @@ public class EventosClimaticos
     [Key]
     public int EventoClimaticoId { get; set; }
 
-    [Required(ErrorMessage = "La fecha es requerida")]
+    [Required(ErrorMessage = "La fecha es obligatoria")]
     public DateTime Fecha { get; set; } = DateTime.Now;
 
-    [Required(ErrorMessage = "La zona es requerida")]
+    [Required(ErrorMessage = "La zona es obligatoria")]
     public string Zona { get; set; } = null!;
 
-    [Required(ErrorMessage = "El tipo de evento es requerido")]
+    [Required(ErrorMessage = "El tipo de evento es obligatorio")]
     public string TipoEvento { get; set; } = null!;
-    [Required(ErrorMessage = "La intensidad es requerida")]
+    [Required(ErrorMessage = "La intensidad es obligatoria")]
     public string Intensidad { get; set; } = null!;
     public string Observaciones { get; set; } = string.Empty;
     public string EmpleadoId { get; set; }
